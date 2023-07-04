d03466dac24601ff10239da8dce5da5bbe08cedc
Guilherme Piangers <eduardomartinspiangers@gmail.com>
2023-07-03 23:37:28 -0300

    create color palette

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
 export default function Home() {
-  return <main className=""></main>
+  return (
+    <main className="">
+      <div className="w-6 h-6 rounded-md bg-light-500"></div>
+      <div className="w-6 h-6 rounded-md bg-priority-lvl1"></div>
+      <div className="w-6 h-6 rounded-md bg-priority-lvl2"></div>
+      <div className="w-6 h-6 rounded-md bg-priority-lvl3"></div>
+      <div className="w-6 h-6 rounded-md bg-priority-lvl4"></div>
+      <div className="w-6 h-6 rounded-md bg-atributeBar"></div>
+    </main>
+  )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,24 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        primary: '#5F7CE3',
+        primaryActive: '#7F98F1',
+        light: {
+          100: '#F9F9F9',
+          200: '#F4F4F4',
+          300: '#F0F0F0',
+          400: '#ECECEC',
+          500: '#D9D9D9',
+        },
+        priority: {
+          lvl1: '#7CD08A',
+          lvl2: '#7089E2',
+          lvl3: '#D6A65E',
+          lvl4: '#D07C7C',
+        },
+        atributeBar: '#D3883E',
+      },
     },
   },
   plugins: [],
