b814c7d60a2869d195a1a43a48b8a01c7e382c3a
Guilherme Piangers <eduardomartinspiangers@gmail.com>
2023-07-04 18:39:34 -0300

    start coding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,11 @@
+"use client";
+
+import Task from "@/components/Task/Task";
+
 export default function Home() {
   return (
-    <main className="">
-      <div className="w-6 h-6 rounded-md bg-light-500"></div>
-      <div className="w-6 h-6 rounded-md bg-priority-lvl1"></div>
-      <div className="w-6 h-6 rounded-md bg-priority-lvl2"></div>
-      <div className="w-6 h-6 rounded-md bg-priority-lvl3"></div>
-      <div className="w-6 h-6 rounded-md bg-priority-lvl4"></div>
-      <div className="w-6 h-6 rounded-md bg-atributeBar"></div>
+    <main className="p-4">
+      <Task></Task>
     </main>
-  )
+  );
 }
