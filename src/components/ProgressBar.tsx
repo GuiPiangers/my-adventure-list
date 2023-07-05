8cc1538a192ba996d460a57233b5f79f7c0832b8
Guilherme Piangers <eduardomartinspiangers@gmail.com>
2023-07-05 18:42:40 -0300

    fix width progressBar

--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -4,16 +4,22 @@ type Props = {
   max?: number;
   value?: number;
   color?: string;
+  width?: string;
 } & HTMLAttributes<HTMLDivElement>;
 
 export default function ProgressBar({
   value = 0,
   max = 100,
   color = "#7F98F1",
+  width = "200px",
   ...rest
 }: Props) {
   return (
-    <div className="h-2 w-80 rounded-full bg-light-500" {...rest}>
+    <div
+      className="h-2 w-80 rounded-full bg-light-500"
+      {...rest}
+      style={{ width }}
+    >
       <div
         className="h-full rounded-full "
         style={{ width: `${(value / max) * 100}%`, backgroundColor: color }}
