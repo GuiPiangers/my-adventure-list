bbcd115ae07bea9cf70eb94635e2f99e116716bb
Guilherme Piangers <eduardomartinspiangers@gmail.com>
2023-07-10 18:43:43 -0300

    import radix range

--- src/components/ui/RangerInput.tsx
+++ src/components/ui/RangerInput.tsx
@@ -1,13 +1,23 @@
-export default function RangerInput() {
-  return (
-    <div>
-      <input
-        className="h-2 w-full appearance-none bg-light-500 "
-        type="range"
-        min={1}
-        max={4}
-        step={1}
+import React from "react";
+import * as Slider from "@radix-ui/react-slider";
+
+const SliderDemo = () => (
+  <form>
+    <Slider.Root
+      className="relative flex h-5 touch-none select-none items-center"
+      defaultValue={[0]}
+      max={3}
+      step={1}
+    >
+      <Slider.Track className="relative h-1 grow rounded-full bg-light-500">
+        <Slider.Range className="absolute h-full rounded-full bg-primary" />
+      </Slider.Track>
+      <Slider.Thumb
+        className="block h-5 w-5 rounded-full border border-primary bg-white focus:outline-primary"
+        aria-label="Volume"
       />
-    </div>
-  );
-}
+    </Slider.Root>
+  </form>
+);
+
+export default SliderDemo;
